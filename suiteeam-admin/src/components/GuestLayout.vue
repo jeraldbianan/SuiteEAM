681533d538c7266c1b21c5c8b03f1b04bbafcace
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-14 00:28:34 +0800

    admin: develop a reusable component GuestLayout

---
+++ suiteeam-admin/src/components/GuestLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import EAMLogo from '@/assets/images/EAM.png';
+
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <div
+    class="flex min-h-screen w-screen items-center justify-center bg-[url('@/assets/images/login-bg.jpg')] bg-cover bg-center bg-no-repeat px-4 md:px-0"
+  >
+    <div
+      class="flex max-w-[400px] flex-1 -translate-y-20 flex-col items-center rounded-lg bg-light p-10"
+    >
+      <img :src="EAMLogo" alt="Neonella Logo" class="h-32 w-32" />
+
+      <h1 class="font-pd text-center text-2xl/3 font-medium leading-none text-dark">
+        {{ title }}
+      </h1>
+
+      <div class="mb-5 mt-8 w-full">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
