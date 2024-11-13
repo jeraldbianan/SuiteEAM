5e61c99bae8c5c4be8194b5a40e758e99b6b7cb0
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-13 18:27:25 +0800

    admin: install and setup vuex, tested and working

--- suiteeam-admin/src/main.js
+++ suiteeam-admin/src/main.js
@@ -1,9 +1,11 @@
-import './assets/main.css';
+import '@/assets/main.css';
 
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import store from '@/store';
+import App from '@/App.vue';
+import router from '@/router';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
--- suiteeam-admin/src/views/LoginView.vue
+++ suiteeam-admin/src/views/LoginView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import store from '@/store';
+import { computed } from 'vue';
+
+const test = computed(() => store.state.test);
+</script>
 
 <template>
-  <div>Login</div>
+  <div>
+    <h1 class="text-5xl">Login</h1>
+
+    {{ test }}
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
