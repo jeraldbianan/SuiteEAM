1ca51809fc3f465ad88936832edf5e694ec0de76
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-13 22:36:15 +0800

    api: create an Admin middleware and implement in routes api

--- suiteeam-api/routes/api.php
+++ suiteeam-api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
