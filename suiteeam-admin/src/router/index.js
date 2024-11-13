a9565ad1ab0de7dc2c7cee5d0f9c14a69e390c83
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-14 03:24:41 +0800

    admin: register some dummy routes for the sidebar

--- suiteeam-admin/src/router/index.js
+++ suiteeam-admin/src/router/index.js
@@ -1,6 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AppLayout from '@/components/AppLayout.vue';
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/app/dashboard',
+  },
+  {
+    path: '/app',
+    name: 'app',
+    component: AppLayout,
+    meta: {
+      requiresAuth: true,
+    },
+    children: [
+      {
+        path: 'dashboard',
+        name: 'app.dashboard',
+        component: () => import('@/views/DashboardView.vue'),
+      },
+      {
+        path: 'employees',
+        name: 'app.employees',
+        component: () => import('@/views/EmployeesView.vue'),
+      },
+    ],
+  },
   {
     path: '/login',
     name: 'login',
