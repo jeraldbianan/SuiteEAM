c5ba30e22403d4f8e160fd717977c30ea909d3e5
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-14 00:29:43 +0800

    admin: develop reusable input, button and loading spinner components and implement in LoginView

---
+++ suiteeam-admin/src/components/ui/CustomButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    required: false,
+    default: 'button',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['click']);
+
+const onClick = () => {
+  emit('click');
+};
+</script>
+
+<template>
+  <button
+    @click="onClick"
+    :type="type"
+    :disabled="disabled"
+    class="flex items-center justify-center rounded-md bg-primary text-sm/6 font-semibold text-light shadow-sm transition-all hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:scale-[.98]"
+    :class="{ 'cursor-not-allowed': disabled, 'hover:bg-primary/60': disabled }"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
---
+++ suiteeam-admin/src/components/ui/InputField.vue
@@ -0,0 +1,11 @@
+<script setup>
+const model = defineModel();
+</script>
+
+<template>
+  <input
+    v-model="model"
+    class="placeholder:text-gray-400 block w-full rounded-md border-0 px-2 py-1.5 text-dark shadow-sm focus:ring-2 focus:ring-primary sm:text-sm/6"
+    v-bind="$attrs"
+  />
+</template>
