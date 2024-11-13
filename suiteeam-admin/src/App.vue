46157e602c9717114b84ec048ad1770bb642216a
Jerald Bianan <jeraldbianan@gmail.com>
2024-11-13 18:00:18 +0800

    admin: added colors and font in tailwind config

--- suiteeam-admin/src/App.vue
+++ suiteeam-admin/src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="text-5xl text-red-700">Hello World!</div>
+  <div class="font-neue text-5xl text-red-700">Hello World!</div>
 </template>
 
 <style lang="scss" scoped></style>
--- suiteeam-admin/tailwind.config.js
+++ suiteeam-admin/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#85012D',
+        secondary: '#D50037',
+        success: '#4CAF50',
+        warning: '#FFC107',
+        info: '#00BCD4',
+        dark: '#0B0D0C',
+        light: '#E8ECEF',
+        gray: '#D9DEE2',
+        'dark-gray': '#353535',
+      },
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
